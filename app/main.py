c3786a560b8d41f4da571760d6dc444e34f6f22e
matheusperches <mperches3@yahoo.com.br>
2024-02-23 19:35:15 -0300

    test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 import os
 import json
 import mlflow
+import uvicorn
 import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI
+
+
+class FetalHealthData(BaseModel):
+    accelerations: float
+    fetal_movement: float
+    uterine_contractions: float
+    severe_decelerations: float
+
 
 app = FastAPI(title="Fetal Health API",
               openapi_tags=[
@@ -19,6 +28,32 @@ app = FastAPI(title="Fetal Health API",
               ])
 
 
+def load_model():
+    print('reading model...')
+    mlflow_tracking_uri = 'https://dagshub.com/renansantosmendes/mlops-ead.mlflow'
+    mlflow_tracking_username = 'renansantosmendes'
+    mlflow_tracking_password = '' # missing access tokens
+    os.environ['mlflow_tracking_username'] = mlflow_tracking_username
+    os.environ['mlflow_tracking_password'] = mlflow_tracking_password
+    print('setting mlflow...')
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    print('Creating client...')
+    client = mlflow.MlflowClient(tracking_uri=mlflow_tracking_uri)
+    print('getting the registered model')
+    registered_model = client.get_registered_model('MODEL_NAME')
+    print('reading model...')
+    run_id = registered_model.latest_versions[-1].run_id
+    logged_model = f'runs:/{run_id}/model'
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+    print(loaded_model)
+    return loaded_model
+
+
+@app.on_event(event_type='startup')
+def startup_event():
+    global loaded_model
+    loaded_model = load_model()
+
 @app.get(path='/',
          tags=['Health'])
 def api_health():
@@ -27,5 +62,16 @@ def api_health():
 
 @app.post(path='/predict',
           tags=['Prediction'])
-def predict():
-    return {"prediction": 0}
+def predict(request: FetalHealthData):
+    global local_model
+
+    received_data = np.array([
+        request.accelerations,
+        request.fetal_movement,
+        request.uterine_contractions,
+        request.severe_decelerations,
+    ]).reshape(1, -1)
+    print(received_data)
+    prediction = loaded_model.predict(received_data)
+    print(prediction)
+    return {"prediction": str(np.argmax(prediction[0]))}
--- app/main_model.py
+++ app/main_model.py
@@ -4,28 +4,3 @@ import mlflow
 import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI
-
-app = FastAPI(title="Fetal Health API",
-              openapi_tags=[
-                  {
-                      "name": "Health",
-                      "description": "Get API Health"
-
-                  },
-                  {
-                      "name": "Prediction",
-                      "description": "Model prediction"
-                  }
-              ])
-
-
-@app.get(path='/',
-         tags=['Health'])
-def api_health():
-    return {"status": "healthy"}
-
-
-@app.post(path='/predict',
-          tags=['Prediction'])
-def predict():
-    return {"prediction": 0}
